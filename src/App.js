ecc25b19b96655fc90b026a3ca749188b85bbff8
smug <supersarath007@gmail.com>
2022-09-11 12:30:23 +0530

    Component

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import React from "react";
+import Header from "./components/Header"
+import Home from "./components/Home"
+
+
+function App() {
+  return <div className="App">
+    <Header />
+    <Header />
+
+    <Home />
+    <Home />
+  </div>;
+
+}
+
+
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Child from './Child'
+const Home = () => {
+  return (
+    <div>
+        <h1>Welcome Home</h1>
+        <Child />
+    </div>
+  )
+}
+
+export default Home
